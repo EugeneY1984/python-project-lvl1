81672b910183445ffbf3d763ce3e07bbca971a2c
eugene.yuriev <juriev_gek@mail.ru>
2022-03-30 17:01:06 +0300

    add step_4

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,9 @@
+"""Set of functions."""
+
 import prompt
 
+
 def welcome_user():
+    """Welcome user my entered name."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games_intro.py
+++ brain_games/scripts/brain_games_intro.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+Hexlet: Project 1.
+
+Studing module
+"""
+
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Run an example code."""
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
